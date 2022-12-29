190ddecdc1b56bad11db606ecb0f7bf23d11052c
Mehmet Akarcorten <97798919+mehmetakarcorten@users.noreply.github.com>
2022-12-29 20:08:36 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,113 @@
+import pygame
+import sys
+import math
+import numpy as np
+import model
+from time import sleep
+
+clamp = lambda a,b,c : min(max(a,b),c)
+
+pygame.init()
+pygame.font.init()
+
+class Information:
+	version = 0.1
+	fps = 120
+	windowSize = (800,600)
+	pause = 0
+	exit = 0
+
+class Text(pygame.Surface):
+	def __init__(self,text="~",size=6,font="Courier",bold=False,italic=False,colour="white"):
+		font = pygame.font.SysFont(font,size,bold,italic)
+		lines = [font.render(i,False,colour) for i in text.replace("\t","    ").split("\n")]
+		height = 0
+		width = 0
+		for i in range(len(lines)):
+			height += lines[i].get_height()
+			width = max(width,lines[i].get_width())
+
+		super(Text,self).__init__((width,height))
+		self.set_colorkey((0,0,0))
+		for i in range(len(lines)):
+			self.blit(lines[i],(0,i*int(height/len(lines))))
+	def run(self):
+		return self
+
+
+class Renderer:
+	def __init__(self, width=800,height=600):
+		self.clock = pygame.time.Clock()
+		self.value = '''{
+			"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,
+			"7":0,"8":0,"9":0}'''
+		pygame.display.set_caption("Digit Recognition")
+		self.window = pygame.display.set_mode((width,height),0,16)
+		icon = pygame.image.load('handwritten.png')
+		pygame.display.set_icon(icon)
+
+		self.positions = []
+		self.frameArray = [[0]*28 for i in range(28)]
+		self.frame = pygame.Surface((28,28))
+
+
+		self.runtime(width=width,height=height)
+
+	def events(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				Information.exit = 1
+			keys = pygame.key.get_pressed()
+			mouse = pygame.mouse.get_pressed()
+
+			if keys[pygame.K_ESCAPE]:
+				Information.pause = 1-Information.pause
+			if not Information.pause:
+				if mouse[0]:
+					pos = list(pygame.mouse.get_pos())
+					pos[0] = int(pos[0]*(28/800))
+					pos[1] = int(pos[1]*(28/600))
+					if pos not in self.positions: self.positions.append(pos)
+				else:
+					self.value = predictModel.predict(array=np.array([self.frameArray]))
+				if keys[pygame.K_c]:
+					self.frameArray = [[0]*28 for i in range(28)]
+					self.positions = []
+				
+
+	def runtime(self,width,height):
+		while not Information.exit:
+			if not Information.pause:
+				self.frame.fill((0,0,0))
+
+				for i in self.positions:
+					self.frame.set_at(i,(255,255,255))
+
+				for y in range(28):
+					for x in range(28):
+						col = self.frame.get_at((x,y))
+						if col == (255,255,255,255):
+							self.frameArray[y][x] = 255
+
+
+				scaled = pygame.transform.scale(self.frame,(width,height))
+				self.window.blit(scaled,(0,0))
+				self.value = self.value.replace(",","\n")
+
+				txt = Text(text=f"{self.value}",size=15).run()
+				self.window.blit(txt,(800-(txt.get_width()),0))
+
+			self.events()
+
+			pygame.display.update()
+			self.clock.tick(Information.fps)
+			pygame.display.flip()
+
+		predictModel.close()
+		pygame.quit()
+		sys.exit()
+
+if __name__ == "__main__":
+	
+	predictModel = model.Model()
+	render = Renderer(Information.windowSize[0],Information.windowSize[1])
---
+++ model.py
@@ -0,0 +1,61 @@
+import os
+import tensorflow
+import numpy as np
+import dotenv
+import shutil
+import tkinter
+import logging
+
+tensorflow.get_logger().setLevel(logging.ERROR)
+
+
+info = dotenv.find_dotenv()
+dotenv.load_dotenv(info)
+
+model_shape = eval(os.environ["MODEL_SHAPE"])
+prev_model_shape = eval(os.environ["PREV_MODEL_SHAPE"])
+epochs = eval(os.environ["EPOCHS"])
+prev_epochs = eval(os.environ["PREV_EPOCHS"])
+
+class Model:
+	def __init__(self):
+		self.activation = True
+		mnist = tensorflow.keras.datasets.mnist
+		(xt,yt),(xt2,yt2) = mnist.load_data()
+
+		xt = tensorflow.keras.utils.normalize(xt,axis=1)
+		xt2 = tensorflow.keras.utils.normalize(xt2,axis=1)
+		if (prev_model_shape != model_shape) or (os.path.isdir("digits.model") == False) or (prev_epochs != epochs):
+			try: shutil.rmtree("digits.model")
+			except: pass
+
+			model = tensorflow.keras.models.Sequential()
+			model.add(tensorflow.keras.layers.Flatten(input_shape=model_shape))
+			model.add(tensorflow.keras.layers.Dense(128,activation="relu"))
+			model.add(tensorflow.keras.layers.Dense(128,activation="relu"))
+			model.add(tensorflow.keras.layers.Dense(10,activation="softmax"))
+
+			model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+			model.fit(xt,yt,epochs=epochs)
+			model.save("digits.model")
+			dotenv.set_key(info,"PREV_MODEL_SHAPE",str(model_shape))
+			dotenv.set_key(info,"PREV_EPOCHS",str(epochs))
+
+		self.model = tensorflow.keras.models.load_model("digits.model")
+		self.loss, self.accuracy = self.model.evaluate(xt2,yt2)
+		print(self.loss, self.accuracy)
+	
+	def predict(self,array):
+		probabilities = {
+		"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0}
+		if self.activation:
+			prediction = self.model.predict(array)
+			for i in range(len(prediction[0])):
+				probabilities[str(i)] = prediction[0][i]
+			return f"This digit is probably: {np.argmax(prediction)}\n"+str(probabilities)
+
+	def close(self):
+		self.activation = False
+
+
+	
